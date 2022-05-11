4104246ff1eb12497107b27d917286b54e4f087b
Dainler <dainler.ms@gmail.com>
2022-05-11 19:25:46 -0400

    CRUD Angular + Spring | 21: Formulário: API Spring: Criar Curso (HTTP POST)

--- src/main/java/com/dainler/controller/CourseController.java
+++ src/main/java/com/dainler/controller/CourseController.java
@@ -5,9 +5,13 @@ import java.util.List;
 import com.dainler.model.Course;
 import com.dainler.repository.CourseRepository;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
@@ -30,6 +34,21 @@ public class CourseController {
   @GetMapping
   public @ResponseBody List<Course> list() {
     return courseRepository.findAll();
+  }
+
+  /*
+   * @PostMapping
+   * public ResponseEntity<Course> create(@RequestBody Course course) {
+   * return
+   * ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course))
+   * ;
+   * }
+   */
+
+  @PostMapping
+  @ResponseStatus(code = HttpStatus.CREATED)
+  public Course create(@RequestBody Course course) {
+    return courseRepository.save(course);
   }
 
 }
